b8f76b0722fb6d6527cc076bf876a7e6aee76733
Manoel <999142818m@gmail.com>
2024-06-25 18:29:59 -0300

    up

--- src/controllers.jl
+++ src/controllers.jl
@@ -1,51 +1,63 @@
 function ControllerGetUserById()
-  id = getId()
-  if isempty(getUser(id))
+  try
+    id = getId()
+    return Json.json(id, status=200)
+  catch
     return Json.json("mesagem" => "User not found", status=404)
   end
-  return Json.json(getUser(id))
 end
 
 function ControllerGetUsers()
-  result = DBInterface.execute(conn,"SELECT * FROM usuario")
-  dadosUser = getUsers(result)
-  if isempty(dadosUser)
-    return Json.json("mesagem" => "No users found", status=404)
+  try
+    result = DBInterface.execute(conn, "SELECT * FROM usuario")
+    dadosUser = getUsers(result)
+    return Json.json(dadosUser, status=200)
+  catch
+    return Json.json("mesagem" => "Error in the database", status=500)
   end
-  return Json.json(dadosUser)
 end
 
 function ControllerCreateUser()
-  userData = jsonpayload()
-  createUser(
-    userData["name"],
-    userData["age"],
-    userData["gender"],
-    userData["email"],
-    userData["password"]
-  )
-  return Json.json("mesagem" => "User created", status=201)
-end
-
-function ControllerUpdateUser()
-  id = getId()
-  if id !== nothing
+  try
     userData = jsonpayload()
-    updateUser(
+    createUser(
       userData["name"],
       userData["age"],
       userData["gender"],
       userData["email"],
-      userData["password"],
+      userData["password"]
     )
-    return json("message" => "User updated successfully", status=200)
-  else
+    return Json.json("mesagem" => "User created", status=201)
+  catch
+    return Json.json("mesagem" => "Error in the create user", status=204)
+  end
+end
+
+function ControllerUpdateUser()
+  try
+    id = getId()
+    if id !== nothing
+      userData = jsonpayload()
+      updateUser(
+        userData["name"],
+        userData["age"],
+        userData["gender"],
+        userData["email"],
+        userData["password"],
+      )
+      return json("message" => "User updated successfully", status=200)
+    end
+  catch
     return json("message" => "User not found", status=404)
   end
 end
 
 function ControllerDeleteUser()
-  delete = DBInterface.prepare(conn, "DELETE FROM usuario WHERE id =?")
-  DBInterface.execute(delete, (getId(),))
-  return Json.json("mensagem" => "User delete successfully", status = 200)
+  try
+    delete = DBInterface.prepare(conn, "DELETE FROM usuario WHERE id =?")
+    DBInterface.execute(delete, (getId(),))
+    return Json.json("mensagem" => "User delete successfully", status=200)
+  catch
+    return Json.json("mensagem" => "User not found", status=404)
+  end
 end
