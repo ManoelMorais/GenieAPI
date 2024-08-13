cb155257f6ccb407b0cae4af83f771ac6a882660
Manoel <999142818m@gmail.com>
2024-08-12 22:18:45 -0300

    add swagger examples

--- src/API.jl
+++ src/API.jl
@@ -1,6 +1,16 @@
 #module API
 
-using DataFrames, Genie, Genie.Router, Genie.Renderer, Genie.Renderer.Json, Genie.Requests, UUIDs, Dates
+using DataFrames, Genie.Renderer, Genie.Renderer.Json, Genie.Requests, UUIDs, Dates
+using Genie, Genie.Router
+using JSON
+# using SwagUI
+# using YAML
+
+# swagger_document = JSON.parsefile("./swagger.json")
+
+# route("/docs") do 
+#     render_swagger(swagger_document)
+# end
 
 route(ControllerGetUser, "/getUser", method="GET")
 route(ControllerCreateUser, "/createUser", method="POST")
