68977ca375861ebd278f0cb2c23763cf713cbfc5
Manoel <999142818m@gmail.com>
2024-08-15 22:15:46 -0300

    add code for read files yaml

--- src/API.jl
+++ src/API.jl
@@ -3,14 +3,15 @@
 using DataFrames, Genie.Renderer, Genie.Renderer.Json, Genie.Requests, UUIDs, Dates
 using Genie, Genie.Router
 using JSON
-# using SwagUI
-# using YAML
+using SwagUI
+using YAML
 
-# swagger_document = JSON.parsefile("./swagger.json")
+swagger_document = YAML.load_file("./swagger.yaml")
+swagger_document_lts = Dict{String, Any}(string(k) => v for (k, v) in swagger_document)
 
-# route("/docs") do 
-#     render_swagger(swagger_document)
-# end
+route("/docs") do 
+    render_swagger(swagger_document_lts)
+end
 
 route(ControllerGetUser, "/getUser", method="GET")
 route(ControllerCreateUser, "/createUser", method="POST")
